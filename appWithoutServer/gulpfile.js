9b93db9d73c445d51ca7dc3ae91b9760a81a6267
stvking <stvking@gmail.com>
2016-06-03 08:38:35 -0600

    created watch task for html changes

--- appWithoutServer/gulpfile.js
+++ appWithoutServer/gulpfile.js
@@ -13,7 +13,7 @@ var config = {
   }
 };
 
-
+// Set up for dev server
 gulp.task('connect', function () {
   connect.server({
     root: ['dist'],
@@ -29,12 +29,17 @@ gulp.task('open', ['connect'], function () {
     .pipe(open({uri: config.devBaseUrl + ':' +config.port + '/'}));
 });
 
-
+// Take take all html files and put them in the dist directory and then reload
 gulp.task('html', function () {
   gulp.src(config.paths.html)
     .pipe(gulp.dest(config.paths.dist))
     .pipe(connect.reload());
 });
 
+// Watch all files on the html path and run the html task when they change
+gulp.task('watch', function () {
+  gulp.watch(config.paths.html, ['html'])
+});
 
-gulp.task('default', ['html', 'open']);
+// Default task
+gulp.task('default', ['html', 'open', 'watch']);
