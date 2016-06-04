22a5d53033c5414c22b72bfd3aaa16f3025bbffc
stvking <tgfbikes@gmail.com>
2016-06-04 08:45:22 -0600

    removed tests for linter

--- appWithoutServer/src/main.js
+++ appWithoutServer/src/main.js
@@ -2,8 +2,6 @@
 
 //bootstrap expects jquery to be in the global namespace
 $ = jQuery = require('jquery');
-test    = 1;
-
 
 // Using commonjs pattern
 var App = console.log('hello from browserify');
