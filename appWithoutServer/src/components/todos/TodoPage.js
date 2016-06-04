8a317b42d9f163a8040a003942f900af1265e718
stvking <tgfbikes@gmail.com>
2016-06-04 11:08:03 -0600

    rendering static data

--- appWithoutServer/src/components/todos/TodoPage.js
+++ appWithoutServer/src/components/todos/TodoPage.js
@@ -5,10 +5,44 @@ var todoApi = require('../../mockApi/todoApi');
 
 
 var Todos = React.createClass({
+
+  getInitialState: function () {
+    return {
+      todos: []
+    };
+  },
+
+  componentWillMount: function () {
+    this.setState({
+      todos: todoApi.getAllTodos()
+    })
+  },
+
   render: function () {
+    var createTodoRow = function (todo) {
+      return (
+        <tr key={todo.id}>
+          <td>{todo.id}</td>
+          <td><a href={"/#todos/" + todo.id}>{todo.title}</a></td>
+          <td>{todo.description}</td>
+        </tr>
+      );
+    };
+
     return (
       <div>
-        <h1>Todos</h1>
+        <h2>Things we need to get done</h2>
+
+        <table className="table">
+          <thead>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+          </thead>
+          <tbody>
+            {this.state.todos.map(createTodoRow, this)}
+          </tbody>
+        </table>
       </div>
     );
   }
