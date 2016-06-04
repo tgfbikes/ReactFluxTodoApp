3211dd8dab839b353f0feb977f0fef4db567a4d4
stvking <tgfbikes@gmail.com>
2016-06-04 10:54:56 -0600

    initial set up of TodoPage component

---
+++ appWithoutServer/src/components/todos/TodoPage.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var React = require('react');
+var todoApi = require('../../mockApi/todoApi');
+
+
+var Todos = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Todos</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = Todos;
