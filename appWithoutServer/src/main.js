2232920dae4c4636c82599118adedaf02dfdeb27
stvking <tgfbikes@gmail.com>
2016-06-04 14:04:25 -0600

    required hashHistory singleton for Router component

--- appWithoutServer/src/main.js
+++ appWithoutServer/src/main.js
@@ -5,46 +5,13 @@ var $, jQuery;
 $ = jQuery = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Home = require('./components/HomePage');
-var Todos = require('./components/todos/TodoPage');
-var About = require('./components/about/AboutPage');
-var Header = require('./components/common/Header');
+var Router = require('react-router').Router;
+var routes = require('./routes');
+var hashHistory = require('react-router').hashHistory;
 
-var App = React.createClass({
-  render: function () {
-    var Child;
-
-    // this prop is the route prop given below
-    switch (this.props.route) {
-      case 'about':
-        Child = About;
-        break;
-      case 'todos':
-        Child = Todos;
-        break;
-      default: Child = Home;
-    }
-
-    return (
-      <div>
-        <Header />
-        <Child />
-      </div>
-    );
-
-  }
-});
-
-function render() {
-  // Get current route
-  var route = window.location.hash.substr(1);
-  // Pass route as a prop to App
-  ReactDOM.render(<App route={route} />, document.getElementById('app'));
-}
-
-// Whenever there is a hash change, execute render function
-window.addEventListener('hashchange', render);
-render();
-
-// The div with the id 'app' is the mounting point of the application
-// ReactDOM.render(<Home />, document.getElementById('app'));
+ReactDOM.render(
+  <Router history={hashHistory}>
+    {routes}
+  </Router>
+, document.getElementById('app')
+);
