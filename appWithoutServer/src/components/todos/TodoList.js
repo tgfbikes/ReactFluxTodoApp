81f097bb6d93858f21c851484b307ec047fa1db2
stvking <tgfbikes@gmail.com>
2016-06-04 12:17:37 -0600

    removed header

---
+++ appWithoutServer/src/components/todos/TodoList.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var React = require('react');
+
+
+var TodoList = React.createClass({
+
+  render: function () {
+    var createTodoRow = function (todo) {
+      return (
+        <tr key={todo.id}>
+          <td>{todo.id}</td>
+          <td><a href={"/#todos/" + todo.id}>{todo.title}</a></td>
+          <td>{todo.description}</td>
+        </tr>
+      );
+    };
+
+    return (
+        <table className="table">
+          <thead>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Description</th>
+          </thead>
+          <tbody>
+            {this.props.todos.map(createTodoRow, this)}
+          </tbody>
+        </table>
+    );
+  }
+
+});
+
+module.exports = TodoList;
