5ed54cc6d73a1bc4a892cfa38823979b23f585c6
stvking <tgfbikes@gmail.com>
2016-06-06 14:53:16 -0600

    initial set up of todoStore

---
+++ appWithoutServer/src/stores/todoStore.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var Dispatcher = require('../dispatcher/Dispatcher');
+var ActionTypes = require('../constants/actionTypes');
+var EventEmitter = require('events').EventEmitter;
+var CHANGE_EVENT = 'change';
+
+
+var TodoStore = Object.assign({}, EventEmitter.prototype, {
+
+  addChangeListener: function (callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  removeChangeListener: function (callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
+
+  emitChange: function () {
+    this.emit(CHANGE_EVENT);
+  }
+});
