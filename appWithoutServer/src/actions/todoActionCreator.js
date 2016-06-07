ad86db8dfc7bede42e9b2bf1211cedc145aa1c63
stvking <tgfbikes@gmail.com>
2016-06-06 20:59:43 -0600

    initial set up of todoActionCreator

---
+++ appWithoutServer/src/actions/todoActionCreator.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Dispatcher = require('../dispatcher/Dispatcher');
+var todoApi = require('../mockApi/todoApi');
+var ActionTypes = require('../constants/actionTypes');
+
+var TodoActionCreator = {
+  // the actual action creator
+  createTodo: function (todo) {
+    var newTodo = todoApi.saveTodo(todo); // Will be AJAX call in real life
+    
+    Dispatcher.dispatch({
+      actionType: ActionTypes.CREATE_TODO,  // This payload is the actual action
+      todo: newTodo
+    });
+  },
+
+  updateTodo: function (todo) {
+    var updatedTodo = todoApi.saveTodo(todo); 
+
+    Dispatcher.dispatch({
+      actionType: ActionTypes.UPDATE_TODO,  
+      todo: updatedTodo
+    });
+  }
+};
+
+module.exports = TodoActionCreator;
