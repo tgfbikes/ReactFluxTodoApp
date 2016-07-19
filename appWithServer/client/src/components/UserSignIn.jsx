3f82b17fb1a703b79044e82110d32ddf9010f865
stvking <tgfbikes@gmail.com>
2016-07-19 15:14:17 -0600

    created UserSignIn component

---
+++ appWithServer/client/src/components/UserSignIn.jsx
@@ -0,0 +1,18 @@
+'use strict';
+
+var React = require('react');
+
+
+var UserSignIn = React.createClass({
+
+  render: function () {
+    return (
+      <div>
+        <h1>Sign In</h1>
+      </div>
+    );
+  }
+
+});
+
+module.exports = UserSignIn;
