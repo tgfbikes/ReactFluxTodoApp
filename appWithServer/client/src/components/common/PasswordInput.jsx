bcf164f0396199e3397d45c5aa05997e04ae6974
stvking <tgfbikes@gmail.com>
2016-07-19 19:11:10 -0600

    initial setup of password input component

---
+++ appWithServer/client/src/components/common/PasswordInput.jsx
@@ -0,0 +1,34 @@
+'use strict';
+
+var React = require('react');
+
+var PasswordInput = React.createClass({
+  
+  render: function () {
+    var wrapperClass = 'form-group';
+    
+    if (this.props.error && this.props.error.length > 0) {
+      wrapperClass += ' ' + 'has-error';
+    }
+    
+    return (
+      <div className={wrapperClass}>
+        <label htmlFor={this.props.name}>{this.props.name}</label>
+        <div className="field">
+          <input type="password"
+             name={this.props.name}
+             className="form-control"
+             placeholder={this.props.placeholder}
+             ref={this.props.name}
+             value={this.props.value}
+             onChange={this.props.onChange}
+           />
+          <div className="input">{this.props.error}</div>
+        </div>
+      </div>
+    );
+  }
+  
+});
+
+module.exports = PasswordInput;
